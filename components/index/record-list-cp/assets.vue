d693f2107ac039678b7644bc312d8696ad614cb5
huangenzi <enzihuang@126.com>
2020-06-04 18:35:29 +0800

    无用注释删除

--- components/index/record-list-cp/assets.vue
+++ components/index/record-list-cp/assets.vue
@@ -9,23 +9,18 @@
                     <p>{{ $t('record.cp.interestsUserHover') }}</p>
                  </div>
                </th>
-               <!-- <th>{{ $t('record.cp.interestsUser') }}</th> -->
                <th>{{ $t('record.cp.balanceUser') }}</th>
                <th>{{ $t('record.cp.marginUser') }}</th>
                <th style="width: 160px;">{{ $t('record.cp.noMoneyTwo') }}</th>
-               <!-- <th style="width: 140px;">{{ $t('record.cp.onMoney') }}</th> -->
                <th>{{ $t('record.cp.freezeVol') }}</th>
               </tr>
             </thead>
             <tbody class="current-entrust">
                   <tr v-if="token">
                     <td>{{ userSumAssert|retainDecimals({decimal: com.valueUnit}) }}</td>
-                    <!-- <td>{{ accounts.available_vol|retainDecimals({decimal: com.valueUnit}) }}</td> -->
-                    <!-- <td>{{ balanceUser|retainDecimals({decimal: com.valueUnit}) }}</td> -->
                     <td>{{ com.haveAssert|retainDecimals({decimal: com.valueUnit}) }}</td>
                     <td>{{ com.imTotal|retainDecimals({decimal: com.valueUnit}) }}</td>
                     <td>{{ com.PNL|retainDecimals({decimal: com.valueUnit}) }}</td>
-                    <!-- <td>{{ accounts.realised_vol|retainDecimals({decimal: com.valueUnit}) }}</td> -->
                     <td>{{ accounts.freeze_vol |retainDecimals({decimal: com.valueUnit}) }} {{ accounts.coin_code }}</td>
                 </tr>
             </tbody>
@@ -33,14 +28,11 @@
       </div>
 </template>
 <script>
-// import Formula from '../../../assets/js/formula/index.js'
-// import Formula from '../../../assets/js/formula/index.js'
 export default {
   name: 'assets',
   data() {
     return {
       userSumAssert: '', // 账户权益
-      balanceUser: '', // 保证金余额
     };
   },
   computed: {
@@ -84,52 +76,14 @@ export default {
     },
   },
   methods: {
-    // // 获取精度
-    // gePrecision(value, unit, integer = 8) {
-    //   let _unit = this.productInfo[unit].lastIndexOf('.') + 1
-    //   return Utils.retainDecimals(value, {integer, decimal: _unit})
-    // },
-    // 获取可用资产
-    // getCanAssert() {
-    //   let assert = this.accounts.available_vol
-    //   let money = this.$store.state.market.longOptions + this.$store.state.market.sortOptions
-    //   return money < 0 ? (assert - money) : assert
-    // },
-    // 获取仓位保证金
-    // getPositions() {
-    //   let total = 0
-    //   this.cabinList.forEach(item => {
-    //     total += Number(item.im)
-    //     // total += this.LongOrSort(item.hold_vol, item.hold_avg_price, item.position_type === 1, item.im)
-    //   })
-    //   return total
-    // },
-    // 未实现盈亏
-    // LongOrSort(vol, openPrice, way, im) {
-    //   if (!this.ticker.fair_price) {
-    //     return 0
-    //   }
-    //   return Formula.LongOrSort(vol, openPrice, this.ticker.fair_price, Formula.contractObj.getContract(this.productInfo), way)
-    // },
-    // 获取价值精度
-    // getValueUnit() {
-    //   return this.productInfo.value_unit.lastIndexOf('.') + 1
-    // },
     // 获取账户权益
     getUserSumAssert() {
       // 可用 + 冻结 + 未实现盈亏
-      // return Number(this.accounts.available_vol) + Number(this.accounts.freeze_vol) + this.com.PNL + this.com.imTotal
       this.userSumAssert = Number(this.accounts.available_vol) + Number(this.accounts.freeze_vol) + this.com.PNL + this.com.imTotal
     },
-    // 获取保证金余额
-    getBalanceUser() {
-      // return Number(this.accounts.available_vol) + Math.min((this.com.positionLoss + this.com.imTotal), 0)
-      this.balanceUser = Number(this.accounts.available_vol) + Math.min((this.com.positionLoss + this.com.imTotal), 0)
-    }
   },
   mounted() {
     this.getUserSumAssert();
-    this.getBalanceUser();
   },
 }
 </script>
